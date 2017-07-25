a63f018d491a29d577483c7d8b14c0313cccf68f
wujian <470290741@qq.com>
2017-07-25 10:34:26 +0800

    算法

--- javatest/src/main/java/com/example/sort/ShellSort.java
+++ javatest/src/main/java/com/example/sort/ShellSort.java
@@ -15,10 +15,10 @@ public class ShellSort extends BaseSort implements SortDao {
     @Override
     public int sort(int[] array) {
         count = 0;
-        int j, x;
+        int x;
         for (int group = (int) Math.ceil(array.length / 2); group > 0; group /= 2) {
             for (int i = 0; i < group; i++){  //循环组的偏移量
-                for (j = i + group; j < array.length; j += group ){ //循环组偏移量串 每次循环排序当前以及之前的对应串
+                for (int j = i + group; j < array.length; j += group ){ //循环组偏移量串 每次循环排序当前以及之前的对应串
                     int temp = array[j];
                     for (x = j - group; x >= 0 && temp < array[x]; x -= group){ //插入排序，占坑方式，插入排序增量是1  这个是group
                         count++;
